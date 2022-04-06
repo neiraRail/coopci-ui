09181fc3f63a833ec04e0e7861b3a7f747c683e6
Rail Neira <neira.rail@gmail.com>
2022-04-06 17:27:57 -0400

    Vista de pagos sin ci

--- src/components/credito/TablaPagosSinCi.vue
+++ src/components/credito/TablaPagosSinCi.vue
@@ -6,27 +6,41 @@
         <v-container>
             <v-row>
                 <v-col v-for="item, index of pagosSinCi" :key="index">
-                    <v-card>
-                        <v-card-title></v-card-title>
-                        <v-card-text>
+                    <v-card width="295">
+                        <v-card-actions>
                             <v-container>
+                            <v-row>
+                            <v-col>
+                                <v-text-field 
+                                    dense 
+                                    label="Nro CI" 
+                                    v-model="item.ciAsignado"
+                                    :rules="[!/([\D])/.test(item.ciAsignado)]"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col>
+                                <v-btn 
+                                    @click="asignar(item)"
+                                    :disabled="!!!item.ciAsignado"
+                                    >Asignar</v-btn></v-col>
+                            </v-row>
+                            </v-container>
+                        </v-card-actions>
+                        <v-card-title>Folio {{item[0].nroFolio}}</v-card-title>
+                        <v-card-text>
                                 <v-row>
-                                    <v-col>C</v-col>
-                                    <v-col>Int</v-col>
-                                    <v-col>Amt</v-col>
-                                    <v-col>Fecha</v-col>
+                                    <v-col cols="2"><b>C</b></v-col>
+                                    <v-col cols="3"><b>Int</b></v-col>
+                                    <v-col cols="3"><b>Amt</b></v-col>
+                                    <v-col cols="4"><b>Fecha</b></v-col>
                                 </v-row>
                                 <v-row v-for="pago, index of item" :key="index">
-                                    <v-col>{{pago.nroCuota}}</v-col>
-                                    <v-col>{{pago.interes}}</v-col>
-                                    <v-col>{{pago.amortizacion}}</v-col>
-                                    <v-col>{{pago.fecha}}</v-col>
+                                    <v-col cols="2">{{pago.nroCuota}}</v-col>
+                                    <v-col cols="3">{{pago.interes}}</v-col>
+                                    <v-col cols="3">{{pago.amortizacion}}</v-col>
+                                    <v-col cols="4">{{pago.fecha}}</v-col>
                                 </v-row>
-                            </v-container>
                         </v-card-text>
-                        <v-card-actions>
-                            <v-btn>OK</v-btn>
-                        </v-card-actions>
                     </v-card>
                 </v-col>
             </v-row>
@@ -34,7 +48,6 @@
     </v-sheet>
 </template>
 <script>
-import {mapState} from "vuex";
 export default {
     data(){
         return {
@@ -42,7 +55,19 @@ export default {
         }
     },
     computed:{
-        ...mapState("creditos", ["pagosSinCi"])
+        pagosSinCi:{
+            get(){
+                return this.$store.state.creditos.pagosSinCi
+            },
+            set(value){
+                this.$store.commit("creditos/setPagosSinCi", value)
+            }
+        }
+    },
+    methods:{
+        asignar(item){
+            this.$store.dispatch("creditos/asignarCi", item)
+        }
     },
     mounted(){
         this.$store.dispatch("creditos/fetchTodosLosPagosSinCi")
